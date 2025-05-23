47b0b84616bc7ac81d81b076e74d8f4207232fb3
runae <tuguran@mail.ru>
2025-05-23 23:41:16 +0300

    ignor

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,6 @@
 fun main() { // объявили тело метода
     println("Hello world!"); // указали действие
     println("It's I'm")
-    println("Anastasiia")
-    println("Mistic")
-    println("Busia")
-    println("Begemote")
-    println("Margo")
+    println("Anastasiia\n" + "Mistic\n" + "Busia\n" + "Begemote\n" + "Margo")
 
 }
