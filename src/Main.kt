af4c0ca6c690b8a97fb2a08134714fb93cab9e93
Stellareign <tuguran@mail.ru>
2025-05-23 22:59:13 +0300

    начало положено

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main() { // объявили тело метода
+    println("Hello world!"); // указали действие
+    println("It's I'm")
+    println("Anastasiia")
+    println("Mistic")
+    println("Busia")
+    println("Begemote")
+    println("Margo")
+
+}
