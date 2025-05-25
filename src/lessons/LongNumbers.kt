c68ac3d776530bfe54b53b9e51f98b86d3256c4b
runae <tuguran@mail.ru>
2025-05-25 22:31:18 +0300

    до 2.15 (Циклы, диапазоны, числа)

--- src/lessons/LongNumbers.kt
+++ src/lessons/LongNumbers.kt
@@ -1,5 +1,11 @@
 package lessons
 
 fun main() {
-
+//    val numbers = 0..1000_000_000;
+//    var sum = 0;
+//    for (number in numbers) {
+//        sum += number;
+//    }
+//    println(sum);
+    val a: Byte = 1;
 }
